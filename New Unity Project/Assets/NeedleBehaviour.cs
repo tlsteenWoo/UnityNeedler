77cdeabbf2b58fb882a23c4fffa7723176ee5eb7
Tim Steen <tls8963@rit.edu>
2015-06-03 16:09:13 -0400

    Create needle impact
    * NeedleCollision.cs
    * Needle box trigger
    * Target box collider

--- New Unity Project/Assets/NeedleBehaviour.cs
+++ New Unity Project/Assets/NeedleBehaviour.cs
@@ -6,6 +6,7 @@ using System.Collections;
 /// It provides logic for flying and expiration.
 /// </summary>
 public class NeedleBehaviour : MonoBehaviour {
+	protected bool isStuck;
 	public float travelSpeed = 1;
 	public float lifeTime = 3;
 	protected float lifeLived;
@@ -28,6 +29,31 @@ public class NeedleBehaviour : MonoBehaviour {
 	}
 
 	/// <summary>
+	/// When a NeedleProjectile Impacts, make it stick into object it hit, and refresh its life.
+	/// Play an impact sound to confirm the hit.
+	/// </summary>
+	/// <param name="host">Host which was impacted bu the needle.</param>
+	public void Impact(Collider host)
+	{
+		isStuck = true;
+		//attach it to the object
+		transform.SetParent (host.gameObject.transform);
+		//Rigid body is destroyed so that no more collisions or forces occur.
+		var rigidBody = GetComponent<Rigidbody> ();
+		Destroy (rigidBody);
+		Refresh ();
+		//TODO: Play impact sound
+	}
+
+	/// <summary>
+	/// Restart this needles life, preventing expiration
+	/// </summary>
+	public void Refresh()
+	{
+		lifeLived = 0;
+	}
+
+	/// <summary>
 	/// Destroys the NeedleProjectile.
 	/// </summary>
 	public void Expire()
@@ -41,6 +67,9 @@ public class NeedleBehaviour : MonoBehaviour {
 	/// </summary>
 	void Fly()
 	{
+		//do not move a needle which is stuck
+		if (isStuck)
+			return;
 		//Advance the needle forward based on direction, speed, and change in time
 		transform.position += transform.forward * travelSpeed * deltaTime;
 	}
---
+++ New Unity Project/Assets/NeedleCollision.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// This component is used to separate needle impact triggers from needle search triggers.
+/// </summary>
+public class NeedleCollision : MonoBehaviour {
+	public NeedleBehaviour needleBehaviour;
+
+	/// <summary>
+	/// If this trigger encounters a non-trigger then trigger needle impact.
+	/// </summary>
+	/// <param name="other">The non-trigger impact collider.</param>
+	public void OnTriggerEnter(Collider other)
+	{
+		if (other.isTrigger)
+			return;
+		needleBehaviour.Impact (other);
+	}
+}
