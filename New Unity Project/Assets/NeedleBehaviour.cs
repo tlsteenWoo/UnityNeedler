151f1c6705c3a5266c42245a27e95c4fd697f613
Tim Steen <tls8963@rit.edu>
2015-06-03 12:41:23 -0400

    Previous work.
    #Log 6/2/2015
    
    * Create placeholder models & animations
    * Create 3D unity project
    * Create terrain. Shrunk to 50x50 w/129 Textures
    ** Create mountains around perimeter
    * Create NeedleProjectile (Basic)
    * Create NeedleSpawner
    * Design Elements: Sounds, Muzzle Flash, GUI, Target Death

---
+++ New Unity Project/Assets/NeedleBehaviour.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// NeedleBehaviour is a component for NeedleProjectiles.
+/// It provides logic for flying and expiration.
+/// </summary>
+public class NeedleBehaviour : MonoBehaviour {
+	public float travelSpeed = 1;
+	public float lifeTime = 3;
+	protected float lifeLived;
+	protected float deltaTime;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		deltaTime = Time.deltaTime;
+		lifeLived += deltaTime;
+		if (lifeLived > lifeTime) {
+			Expire ();
+		} else {
+			Fly ();
+		}
+	}
+
+	/// <summary>
+	/// Destroys the NeedleProjectile.
+	/// </summary>
+	public void Expire()
+	{
+		//destroy the NeedleProjectile gameObject this is attached to
+		DestroyImmediate (this.gameObject);
+	}
+
+	/// <summary>
+	/// The needle will fly forward, based on travel speed and change in time.
+	/// </summary>
+	void Fly()
+	{
+		//Advance the needle forward based on direction, speed, and change in time
+		transform.position += transform.forward * travelSpeed * deltaTime;
+	}
+
+}
---
+++ New Unity Project/Assets/NeedleSpawner.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NeedleSpawner : MonoBehaviour {
+	public GameObject Needle;
+	public bool automatic = true;
+	public bool infiniteAmmo;
+	public float spreadAngleYaw = 10;
+	public float spreadAnglePitch = 10;
+	protected bool fireReady;
+	protected int ammo = 10;
+	public int ammoLimit = 40;
+	public float fireRate = 1;
+	protected float fireRateProgress = 1;
+	/// <summary>
+	/// Returns the ammo available, and sets the ammo within the range [0, ammoLimit]
+	/// </summary>
+	/// <value>The ammo.</value>
+	public int Ammo
+	{
+		get{ return ammo;}
+		set{ ammo = Mathf.Clamp(value,0,ammoLimit);}
+	}
+
+	// Use this for initialization
+	void Start () {
+		//throw an error if needle is not defined
+		if (Needle == null)
+			throw new UnityException ("NeedleSpawner: Needle is not defined.");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Udate the fireRate delay
+		if (fireRateProgress < fireRate) {
+			fireRateProgress += Time.deltaTime;
+		}
+		if(fireRateProgress >= fireRate)
+		{
+			fireReady = true;
+			//set progress to the fire rate so progress won't go over 100%
+			fireRateProgress = fireRate;
+		}
+
+		//refill ammo if we have infinite
+		if (infiniteAmmo)
+			ammo = ammoLimit;
+
+		//Try to automatically trigger
+		if (automatic) {
+			Trigger();
+		}
+	}
+
+	/// <summary>
+	/// Attempts to create a new needle at this position and with a direction within the cone.
+	/// </summary>
+	public bool Trigger()
+	{
+		//Check for ammo
+		if (ammo == 0)
+			return false;
+		//Check if the spawner is fire ready
+		if (!fireReady)
+			return false;
+
+		//determine the needles properties
+		Vector3 needlePosition = transform.position;
+		Quaternion needleDirection = transform.rotation;
+		//Spawn the needle and store a reference
+		GameObject newNeedle = (GameObject)Instantiate (Needle, needlePosition, needleDirection);
+		//randomize the yaw(x) and pitch(y)
+		//multiply by .5 to make the spreadAngle variables accurate to the range
+		Vector2 randomCircle = Random.insideUnitCircle;
+		float randomYaw = randomCircle.y * spreadAngleYaw * 0.5f;
+		float randomPitch = randomCircle.x * spreadAnglePitch * 0.5f;
+		newNeedle.transform.Rotate (randomYaw, randomPitch, 0, Space.Self);
+
+		//now restart the fireRate delay and decrement the ammo
+		fireRateProgress = 0;
+		fireReady = false;
+		ammo--;
+
+		return true;
+	}
+}
