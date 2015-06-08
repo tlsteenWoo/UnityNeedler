76690d35212b7dc8f956429d1885d82a16d81e7a
Tim Steen <tls8963@rit.edu>
2015-06-07 21:56:57 -0400

    Create Needler Character
    * Needlerer prefab
    ** Shoulders
    *** HandR
    *** HandL
    * NeedlerCharacter.cs
    ** Turn
    ** Look
    ** LookAt
    ** TurnRate
    ** LookRate
    *** Look Limit Degrees
    ** Test:
    *** Automatically Turn and Look
    *** Ensure needler shoots in direction
    *** Ensure look limitations acknowledged
    * Needler Projectile changes
    ** Line Renderer added

--- New Unity Project/Assets/NeedlerCharacter.cs
+++ New Unity Project/Assets/NeedlerCharacter.cs
@@ -7,13 +7,22 @@ using System.Collections;
 /// It also carries reserve ammunition for the needle spawners.
 /// </summary>
 public class NeedlerCharacter : MonoBehaviour {
-	public NeedlerHolder needlerHolderR, needlerHolderL;
+	public bool isPlanar = true;
 	public int reserveAmmo = 100;
 	public int reserveAmmoLimit = 200;
+	public float turnRateDeg = 1;
+	public float lookRateDeg = 1;
+	//This must be less than 180
+	public float lookLimitDeg = 160;
+	public GameObject shoulders;
+	public NeedlerHolder needlerHolderR;
+	public NeedlerHolder needlerHolderL;
+	public NeedleHost target;
+	public float turnDegrees;
+	public float lookDegrees;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Awake () {
 	}
 
 	/// <summary>
@@ -88,11 +97,74 @@ public class NeedlerCharacter : MonoBehaviour {
 	}
 
 	/// <summary>
+	/// Rotates the character on the Y axis
+	/// </summary>
+	/// <param name="direction">Direction, any number.</param>
+	public void Turn(float direction)
+	{
+		turnDegrees += turnRateDeg * direction;
+	}
+
+	/// <summary>
+	/// Rotates the shoulders on the X axis
+	/// </summary>
+	/// <param name="direction">Direction, any number.</param>
+	public void Look(float direction)
+	{
+		lookDegrees += lookRateDeg * direction;
+		//split in half so we can accurately clamp the range around the center
+		float lookLimitHalf = (lookLimitDeg / 2);
+		//clamp the shoulder x rotation in the range [-lookLimit/2 , lookLimit/2 ]
+		lookDegrees = Mathf.Clamp(lookDegrees, -lookLimitHalf, lookLimitHalf);
+	}
+
+	/// <summary>
+	/// Causes the shoulders to point towards a point, using look and turn.
+	/// </summary>
+	/// <param name="target">Target point.</param>
+	public void LookAt(Vector3 target)
+	{
+		Vector3 toTarget = target - shoulders.transform.position;
+		Vector3 directionToTarget = toTarget.normalized;
+		
+		//Tilt the character to find targets above and below
+		//Get the dot product on the vertical axis to determine eye level
+		float upDifference = Vector3.Dot (shoulders.transform.up, directionToTarget);
+		//not sure why negation is neccessary but it is
+		float eyeLevel = Mathf.Clamp (-upDifference, -1, 1);
+		if (eyeLevel > 0)
+			Look (eyeLevel);
+		else if(eyeLevel < 0)
+			Look (eyeLevel);
+
+		//Turn the character to find targets right and left
+		//Flatten the two directions, right and toTarget, so they lie on the ground plane
+		Vector2 planarTargetDirection = new Vector2 (directionToTarget.x, directionToTarget.z);
+		Vector2 planarRight = new Vector2 (shoulders.transform.right.x, shoulders.transform.right.z);
+		//check whether the target direction points to the right of the shoulders
+		float horizontal = Vector2.Dot (planarRight, planarTargetDirection);
+		if (horizontal < 0) {
+			Turn (horizontal);
+		} else if (horizontal > 0) {
+			Turn (horizontal);
+		}
+	}
+
+	/// <summary>
 	/// Automatically reload empty needlers
 	/// </summary>
 	void Update () {
 		//TODO: Remove this test
 		Shoot (0);
+		if (target != null) {
+			Vector3 targetPos = target.transform.position;
+			if(target.representative)
+			{
+				targetPos = target.representative.bounds.center;
+			}
+			LookAt (targetPos);
+		}
+		UpdateOrientation ();
 		//TODO: Manage reserve ammo in a property
 		reserveAmmo = Mathf.Clamp (reserveAmmo, 0, reserveAmmoLimit);
 		AutomaticReload (needlerHolderL);
@@ -108,5 +180,26 @@ public class NeedlerCharacter : MonoBehaviour {
 			return;
 		if (a_holder.GetOrFindNeedlerBehaviour ().spawner.Ammo == 0)
 			reloadIndividual (a_holder);
+	}
+	/// <summary>
+	/// Keeps the character planar and limits the shoulders up/down
+	/// </summary>
+	void UpdateOrientation()
+	{
+		//operate on character rotation
+		var charEuler = transform.eulerAngles;
+		if (isPlanar) {
+			charEuler.z = 0;
+			charEuler.x = 0;
+		}
+		charEuler.y = turnDegrees;
+		transform.eulerAngles = charEuler;
+		//operate on shoulder rotation
+		var shoulderEuler = shoulders.transform.localEulerAngles;
+		shoulderEuler.x = lookDegrees;
+		//set these to 0 to ensure proper targeting
+		shoulderEuler.y = 0;
+		shoulderEuler.z = 0;
+		shoulders.transform.localEulerAngles = shoulderEuler;
 	}
 }
